5a2acbc218c36f57b2a9079ae9877133c865c9b2
Kara Woo <woo.kara@gmail.com>
2018-06-04 13:33:57 -0700

    Install and load necessary packages

--- global.R
+++ global.R
@@ -1,1 +1,23 @@
-library("shiny")
+#####################################
+####  Install and load packages  ####
+#####################################
+
+use_package <- function(p,
+                        github,
+                        repos = c(
+                          CRAN = "https://cran.rstudio.com/",
+                          Sage = "https://sage-bionetworks.github.io/ran"
+                        ),
+                        ...) {
+  if (!missing(github)) {
+    devtools::install_github(paste(github, p, sep = "/"), ...)
+  }
+  if (!p %in% installed.packages()[, "Package"]) {
+    install.packages(p, repos = repos, dependencies = TRUE)
+  }
+  library(p, character.only = TRUE)
+}
+
+cran_packages <- c("devtools", "shiny")
+lapply(cran_packages, use_package)
+use_package("dccvalidator", github = "Sage-Bionetworks")
