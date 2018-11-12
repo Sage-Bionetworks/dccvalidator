9a5193dea9f8c3b4bc7427264dc96e808cb71e96
Kara Woo <woo.kara@gmail.com>
2018-11-12 11:00:10 -0800

    Tests for check_annotation_keys()

---
+++ tests/testthat/test-check-annotation-keys.R
@@ -0,0 +1,40 @@
+context("test-check-annotation-keys.R")
+
+test_that("check_annotation_keys returns character(0) when no invalid annotations present", {
+  dat1 <- data.frame()
+  dat2 <- data.frame(assay = "rnaSeq")
+  res1 <- check_annotation_keys(dat1)
+  res2 <- check_annotation_keys(dat2)
+  expect_equal(res1, character(0))
+  expect_equal(res2, character(0))
+})
+
+test_that("check_annotation_keys returns invalid annotation values", {
+  dat <- data.frame(a = 1, b = 2)
+  suppressMessages(res <- check_annotation_keys(dat))
+  expect_equal(res, names(dat))
+})
+
+test_that("check_annotation_keys provides message", {
+  dat <- data.frame(a = 1, b = 2)
+  expect_message(check_annotation_keys(dat))
+})
+
+test_that("check_annotation_keys works for File objects", {
+  skip_on_travis()
+  skip_on_cran()
+  library("synapser")
+  synLogin()
+  a <- synGet("syn17038064", downloadFile = FALSE)
+  b <- synGet("syn17038065", downloadFile = FALSE)
+  resa <- suppressMessages(check_annotation_keys(a))
+  resb <- suppressMessages(check_annotation_keys(b))
+  expect_equal(resa, character(0))
+  expect_equal(resb, "randomAnnotation")
+})
+
+test_that("check_annotation_keys works for file views", {
+  fv <- synTableQuery("SELECT * FROM syn17038067")
+  res <- suppressMessages(check_annotation_keys(fv))
+  expect_equal(res, "randomAnnotation")
+})
