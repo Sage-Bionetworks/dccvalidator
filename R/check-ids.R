6660a832d6b074992d1860178a9a3c54b263fb9e
Kara Woo <kara.woo@sagebase.org>
2019-02-18 11:17:30 -0800

    Take one single approach to comparing specimen and individual IDs
    check_ids() compares two data frames looking differences in `idcol`.
    check_specimen_ids() sets idcol to "specimenID"; check_indiv_ids() sets it to
    "individualID". This seems like the best simple approach and will let these
    functions compare data frames of metadata (e.g. individual/biospecimen metadata,
    or biospecimen/assay metadata) but also allows checking a metadata file against
    a manifest or file view, provided these are read into R as data frames. We might
    expand this later to be able to see all 3 at once.

--- R/check-ids.R
+++ R/check-ids.R
@@ -1,33 +1,55 @@
-#' Check individual IDs
+#' Check ids
 #'
-#' Ensure that all individual IDs in two data frames match.
-#'  
-#' @param x,y Data frames of metadata to compare
-#' @return A list of individual IDs missing from x (but present in y) and
-#'   missing from y (but present in x)
+#' Compare IDs (such as individual IDs or specimen IDs) between two data frames.
+#'
+#' @param x,y Data frames to compare
+#' @param idcol Name of column containing ids to compare
+#' @return List of IDs missing from x (but present in y) and missing from y (but
+#'   present in x)
 #' @export
-check_indiv_ids <- function(x, y) {
-  if (!"individualID" %in% colnames(x) | !"individualID" %in% colnames(y)) {
+check_ids <- function(x, y, idcol = c("individualID", "specimenID")) {
+  if (!idcol %in% colnames(x) | !idcol %in% colnames(y)) {
     stop(
-      "Both x and y must contain an `individualID` column",
+      paste0("Both x and y must contain column ", idcol),
       call. = FALSE
     )
   }
 
   ## Ensure that factor columns are coerced to character
-  if (is.factor(x$individualID)) {
-    x$individualID <- as.character(x$individualID)
+  if (is.factor(x[[idcol]])) {
+    x[[idcol]] <- as.character(x[[idcol]])
   }
-  if (is.factor(y$individualID)) {
-    y$individualID <- as.character(y$individualID)
+  if (is.factor(y[[idcol]])) {
+    y[[idcol]] <- as.character(y[[idcol]])
   }
 
-  missing_from_x <- setdiff(y$individualID, x$individualID)
-  missing_from_y <- setdiff(x$individualID, y$individualID)
-  
+  missing_from_x <- setdiff(y[[idcol]], x[[idcol]])
+  missing_from_y <- setdiff(x[[idcol]], y[[idcol]])
+
   list(
     missing_from_x = missing_from_x,
     missing_from_y = missing_from_y
   )
 }
 
+#' Check individual IDs
+#'
+#' Ensure that all individual IDs in two data frames match.
+#'
+#' @inheritParams check_ids
+#' @export
+#' @rdname check_ids
+check_indiv_ids <- function(x, y) {
+  check_ids(x, y, "individualID")
+}
+
+#' Check specimen IDs
+#'
+#' Ensure that all specimen IDS in two data frames match
+#'
+#' @inheritParams check_ids
+#' @export
+#' @rdname check_ids
+check_specimen_ids <- function(x, y) {
+  check_ids(x, y, "specimenID")
+}
