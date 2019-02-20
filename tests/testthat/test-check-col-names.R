6c4ce20c7ede8d78c67af4d295925fea3dfec751
Kara Woo <kara.woo@sagebase.org>
2019-02-20 11:24:45 -0800

    Add tests

--- tests/testthat/test-check-col-names.R
+++ tests/testthat/test-check-col-names.R
@@ -1,5 +1,7 @@
 context("test-check-col-names.R")
 
+library("synapser")
+
 test_that("check_col_names returns empty character vector when all columns present", {
   template <- data.frame(x = 1, y = 1)
   dat <- data.frame(x = 5:10, y = 5:10)
@@ -13,15 +15,15 @@ test_that("check_col_names returns vector of missing columns", {
 })
 
 
+test_that("get_template fails when not logged in to Synapse", {
+  synLogout()
+  expect_error(get_template("biospecimen"))
+})
+
+if (on_travis()) syn_travis_login() else synLogin()
+
 test_that("check_cols_individual works for individual columns", {
   skip_on_cran()
-
-  library("synapser")
-  if (on_travis()) {
-    syn_travis_login()
-  } else {
-    synLogin()
-  }
 
   cols <- get_template("human")
   full_col_indiv <- data.frame(matrix(ncol = length(cols)))
@@ -38,21 +40,33 @@ test_that("check_cols_individual works for individual columns", {
   )
 })
 
-test_that("check_cols_assay works for assay columns", {
+test_that("check_cols_biospecimen works for biospecimen columns", {
   skip_on_cran()
 
-  library("synapser")
-  if (on_travis()) {
-    syn_travis_login()
-  } else {
-    synLogin()
-  }
+  biosp_names <- get_template("biospecimen")
+
+  full_col_biosp <- data.frame(matrix(ncol = length(biosp_names)))
+  colnames(full_col_biosp) <- biosp_names
+  incomplete_col_biosp <- full_col_biosp[, !names(full_col_biosp) == "organ"]
+
+  expect_equal(
+    check_cols_biospecimen(full_col_biosp),
+    character(0)
+  )
+  expect_equal(
+    check_cols_biospecimen(incomplete_col_biosp),
+    "organ"
+  )
+})
+
+test_that("check_cols_assay works for assay columns", {
+  skip_on_cran()
 
   rnaseq_names <- get_template("rnaSeq")
 
   full_col_assay <- data.frame(matrix(ncol = length(rnaseq_names)))
   colnames(full_col_assay) <- rnaseq_names
-  incomplete_col_assay <- full_col_assay[, !names(full_col_assay) %in% c("tissue", "RIN")]
+  incomplete_col_assay <- full_col_assay[, !names(full_col_assay) == "RIN"]
 
   expect_equal(
     check_cols_assay(full_col_assay, "rnaSeq"),
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,8 @@
 context("utils.R")
 
+library("synapser")
+if (on_travis()) syn_travis_login() else synLogin()
+
 test_that("on_travis() returns TRUE on Travis", {
   expect_equal(on_travis(), isTRUE(as.logical(Sys.getenv("TRAVIS"))))
 })
@@ -12,4 +15,13 @@ test_that("login works on travis", {
   }
   login <- try(syn_travis_login(), silent = TRUE)
   expect_false(inherits(login, "try-error"))
+})
+
+test_that("get_annotation fails if no key provided", {
+  expect_error(get_annotation("syn12345"))
+})
+
+test_that("get_annotation gets the value of an annotation on a Synapse entity", {
+  annot <- get_annotation("syn17038064", "fileFormat")
+  expect_equal(annot, c(syn17038064 = "txt"))
 })
