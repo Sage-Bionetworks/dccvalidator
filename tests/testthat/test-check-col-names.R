db19f10ddff7eb727e1081b8996b5f84e50fe6ba
Kara Woo <kara.woo@sagebase.org>
2019-04-26 16:50:35 -0700

    Set template versions for tests to ensure consistent behavior

--- tests/testthat/test-check-col-names.R
+++ tests/testthat/test-check-col-names.R
@@ -25,7 +25,7 @@ if (on_travis()) syn_travis_login() else synLogin()
 test_that("check_cols_individual works for individual columns", {
   skip_on_cran()
 
-  cols <- get_template("syn12973254")
+  cols <- get_template("syn12973254", version = 1)
   full_col_indiv <- data.frame(matrix(ncol = length(cols)))
   colnames(full_col_indiv) <- cols
   incomplete_col_indiv <- full_col_indiv[, !names(full_col_indiv) %in% "yearsEducation"]
@@ -43,7 +43,7 @@ test_that("check_cols_individual works for individual columns", {
 test_that("check_cols_biospecimen works for biospecimen columns", {
   skip_on_cran()
 
-  biosp_names <- get_template("syn12973252")
+  biosp_names <- get_template("syn12973252", version = 3)
 
   full_col_biosp <- data.frame(matrix(ncol = length(biosp_names)))
   colnames(full_col_biosp) <- biosp_names
@@ -62,7 +62,7 @@ test_that("check_cols_biospecimen works for biospecimen columns", {
 test_that("check_cols_assay works for assay columns", {
   skip_on_cran()
 
-  rnaseq_names <- get_template("syn12973256")
+  rnaseq_names <- get_template("syn12973256", version = 2)
 
   full_col_assay <- data.frame(matrix(ncol = length(rnaseq_names)))
   colnames(full_col_assay) <- rnaseq_names
@@ -93,8 +93,8 @@ test_that("get_template errors for files that are not xlsx or csv", {
 })
 
 test_that("get_template can read in excel and csv templates", {
-  csv <- get_template("syn18384877")
-  xlsx <- get_template("syn18384878")
+  csv <- get_template("syn18384877", version = 1)
+  xlsx <- get_template("syn18384878", version = 1)
   expect_equal(csv, c("a", "b", "c"))
   expect_equal(xlsx, c("a", "b", "c"))
 })
