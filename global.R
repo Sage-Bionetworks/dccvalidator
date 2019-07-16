d85071a28519af535cc3c36eaf56094439a31b8a
Kara Woo <kara.woo@sagebase.org>
2019-07-16 11:24:58 -0700

    Improve formatting in details for data that's a list
    Fixes #29
    
    Not using DT because for some reason it doesn't want to show up. If I include
    the table as a DT table and open the details drawer, I see nothing *until* I
    open the chrome developer tools. Then it shows up, but is too wide (and altering
    fillContainer doesn't seem to fix things satisfactorily). Will stick with a
    static table for now and if we need the DT functionality later we can revisit.

--- global.R
+++ global.R
@@ -10,6 +10,7 @@ library("purrr")
 library("emo")
 library("dccvalidator")
 library("shinyBS")
+library("tibble")
 
 #####################
 ####  Functions  ####
@@ -24,7 +25,7 @@ report_result <- function(result, emoji_prefix = NULL, verbose = FALSE) {
         emo::ji(emoji_prefix),
         result$message,
         ## Include details drawer for verbose == TRUE
-        tags$details(paste0(result$data, collapse = ", "))
+        tags$details(show_details(result$data))
       ),
       popify(
         tags$a(icon(name = "question-circle"), href = "#"),
@@ -55,4 +56,23 @@ report_result <- function(result, emoji_prefix = NULL, verbose = FALSE) {
 
 report_results <- function(results, ...) {
   map(results, report_result, ...)
+}
+
+## Methods for showing additional details about the errors
+show_details <- function(x) {
+  UseMethod("show_details", x)
+}
+
+## By default, just show the values separated by commas
+show_details.default <- function(x) {
+  paste0(x, collapse = ", ")
+}
+
+## If data is a list, convert to a table where there is one column for the names
+## of the elements, and a second column for the comma-separated values
+show_details.list <- function(x) {
+  dat <- map2_dfr(names(x), x, function(y, z) {
+    tibble(key = y, value = paste0(z, collapse = ", "))
+  })
+  renderTable(dat, colnames = FALSE)
 }
