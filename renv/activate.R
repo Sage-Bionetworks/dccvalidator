47ace8b916a1e84a0ce9be6f1bd5a0bc5c855f50
Kara Woo <kara.woo@sagebase.org>
2019-09-23 11:54:07 -0700

    Upgrade renv and some dependencies

--- renv/activate.R
+++ renv/activate.R
@@ -2,7 +2,7 @@
 local({
 
   # the requested version of renv
-  version <- "0.7.0-94"
+  version <- "0.7.0-99"
 
   # signal that we're loading renv during R startup
   Sys.setenv("RENV_R_INITIALIZING" = "true")
