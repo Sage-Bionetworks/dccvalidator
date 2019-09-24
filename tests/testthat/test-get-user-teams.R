4041cff7eeec48c249569a241e50c316aa2fa463
Kara Woo <kara.woo@sagebase.org>
2019-09-24 14:48:52 -0700

    Fix test

--- tests/testthat/test-get-user-teams.R
+++ tests/testthat/test-get-user-teams.R
@@ -4,6 +4,7 @@ library("synapser")
 if (on_travis()) syn_travis_login() else synLogin()
 
 test_that("get_user_teams gets team memberships", {
-  teams <- get_user_teams("dcctravistest")
+  user <- synapser::synGetUserProfile("dcctravistest")
+  teams <- get_user_teams(user)
   expect_equal(teams, "3396691")
 })
