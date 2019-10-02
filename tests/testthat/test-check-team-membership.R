ef75bd65fcc0ee3737c447336bb1e1ce6a6d153c
Kara Woo <karawoo@users.noreply.github.com>
2019-10-02 13:37:32 -0700

    Allow checking membership in multiple teams (#84)
    * Allow checking membership in multiple teams
    
    * Simplify report_missing_membership()

--- tests/testthat/test-check-team-membership.R
+++ tests/testthat/test-check-team-membership.R
@@ -3,14 +3,20 @@ context("test-check-team-membership.R")
 library("synapser")
 if (on_travis()) syn_travis_login() else synLogin()
 
-test_that("check_team_membership() does nothing if user is in the team", {
+test_that("check_team_membership() returns check_pass if user is in the team", {
   user <- synapser::synGetUserProfile("dcctravistest")
-  result <- check_team_membership(team = "3396691", user = user)
-  expect_null(result)
+  result <- check_team_membership(teams = "3396691", user = user)
+  expect_true(inherits(result, "check_pass"))
 })
 
-## TODO: test the behavior when a user *is* in the team. This requires a shiny
-## session in which to show the dialog. It'll be something to address as part of
-## https://github.com/Sage-Bionetworks/dccvalidator/issues/56 -- how to test the
-## app itself. We may want to make some mini apps with some of these components
-## for testing.
+test_that("check_team_membership() returns check_fail if user not in team", {
+  user <- synapser::synGetUserProfile("dcctravistest")
+  result <- check_team_membership(teams = "3397398", user = user)
+  expect_true(inherits(result, "check_fail"))
+})
+
+test_that("check_team_membership() can check multiple teams", {
+  user <- synapser::synGetUserProfile("dcctravistest")
+  result <- check_team_membership(teams = c("3397398", "3377637"), user = user)
+  expect_true(inherits(result, "check_fail"))
+})
