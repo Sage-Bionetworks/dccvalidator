c555bb16c1eda1edd424bc7aee6b8cec86f1085d
Aryllen <nmekauer@uw.edu>
2019-10-07 12:49:05 -0700

    Changed the modules directory casing to match others
    The other directories have lowercase names, except for R. Changed Modules to modules to match.

--- app.R
+++ app.R
@@ -2,9 +2,9 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-source("R/Modules/button-indicator.R")
-source("R/Modules/get-study-names.R")
-source("R/Modules/upload-documentation.R")
+source("R/modules/button-indicator.R")
+source("R/modules/get-study-names.R")
+source("R/modules/upload-documentation.R")
 
 pkgload::load_all()
 options( "golem.app.prod" = TRUE)
