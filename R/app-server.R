48e05f485e715fa0043c168af2b5cb1f844bbecc
Aryllen <nmekauer@uw.edu>
2019-10-09 11:54:22 -0700

    Fixing conflict with master
    Team membership was updated in master, but I was working on a different branch before creating this branch. When I brought the changes with me, it had a conflict with the master due to the changing of the team and project ids with the new format.

--- R/app-server.R
+++ R/app-server.R
@@ -19,20 +19,16 @@ app_server <- function(input, output, session) {
     ## Check if user is in AMP-AD Consortium team (needed in order to create
     ## folder at the next step)
     user <- synapser::synGetUserProfile()
-    user_teams <- synapser::synRestGET(paste0(
-      "/user/",
-      user$ownerId,
-      "/team?limit=10000"
-    ))$results
-    user_teams_ids <- purrr::map_chr(user_teams, function(x) x$id)
+    membership <- check_team_membership(teams = c("3320424"), user = user)
+    report_missing_membership(membership)
 
-    if (!"3397129" %in% user_teams_ids) {
-      showModal(
-        modalDialog(
-          title = "Not in AMP-AD Consortium team",
-          # nolint start
-          HTML("You must be a member of the AMP-AD Consortium team on Synapse to use this tool. If you are not a member of the AMP-AD Consortium team, you can request to be added at <a href=\"https://www.synapse.org/#!Team:3320424\">https://www.synapse.org/#!Team:3320424</a>.")
-          # nolint end
+    ## If user is a member of the team(s), create folder to save files and
+    ## enable inputs
+    if (inherits(membership, "check_pass")) {
+      created_folder <- try(
+        create_folder(
+          parent = "syn20506363",
+          name = user$userName
         )
       )
     }
