03deabef51785ecff9ccf9f6ad49815087f9bed6
Nicole Kauer <nmekauer@uw.edu>
2019-10-10 10:47:18 -0700

    Check for complete columns (#97)
    * Team alteration
    
    Altered the team number and folder to upload to for testing.
    
    * Fixing conflict with master
    
    Team membership was updated in master, but I was working on a different branch before creating this branch. When I brought the changes with me, it had a conflict with the master due to the changing of the team and project ids with the new format.
    
    * Added initial check-cols-complete.R and tests
    
    * Removed leftover from conflict
    
    Not sure why this section stuck around after the conflict resolution. Removing it since it's unnecessary.
    
    * Updated example
    
    * Stopped dropping of dataframe when one column or value
    
    * Linting & styler changes
    
    * Edited behavior to correctly list the columns that should be complete
    
    Takes in the `required_cols` list and prints it in the message saying they should be complete.
    
    * Changed `strict` default to `TRUE`
    
    * Updated tests for having the new `strict` default as `TRUE`
    
    * Update comment R/check-cols-complete.R for clarity
    
    Co-Authored-By: Kara Woo <karawoo@users.noreply.github.com>
    
    * Update description of required_cols param of check_cols_complete() for clarity
    
    * Update documentation
    
    Ran `devtools::documentation()` to update documentation.

---
+++ tests/testthat/test-check-cols-complete.R
@@ -0,0 +1,38 @@
+context("test-check-cols-complete.R")
+
+test_that("check_cols_complete returns NULL if given NULL input", {
+  expect_null(check_cols_complete(NULL))
+})
+
+test_that("check_cols_complete returns check_pass if required columns are complete", { #nolint
+  dat <- data.frame(x = 1:2, y = c("a", "b"), z = c(TRUE, FALSE))
+  res <- check_cols_complete(dat, c("x", "y", "z"))
+  expect_true(inherits(res, "check_pass"))
+})
+
+test_that("check_cols_complete returns check_fail if any required column lacks data", { #nolint
+  dat1 <- data.frame(x = NA)
+  dat2 <- data.frame(x = "")
+  dat3 <- data.frame(x = 1, y = NA, z = "")
+  res1 <- check_cols_complete(dat1, "x")
+  res2 <- check_cols_complete(dat2, "x")
+  res3 <- check_cols_complete(dat3, c("x", "y", "z"))
+  expect_true(inherits(res1, "check_fail"))
+  expect_true(inherits(res2, "check_fail"))
+  expect_true(inherits(res3, "check_fail"))
+  expect_equal(res1$data, "x")
+  expect_equal(res2$data, "x")
+  expect_equal(res3$data, c("y", "z"))
+})
+
+test_that("`strict` argument causes check_warn instead of check_fail", {
+  dat <- data.frame(x = NA)
+  res <- check_cols_complete(dat, "x", strict = FALSE)
+  expect_true(inherits(res, "check_warn"))
+})
+
+test_that("can customize empty values", {
+  dat <- data.frame(x = "")
+  res <- check_cols_complete(dat, "x", empty_values = NA)
+  expect_true(inherits(res, "check_pass"))
+})
