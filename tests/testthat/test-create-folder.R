bd833f865e40fdd9dfcacdb8340d9bb53c1c16c5
Kara Woo <karawoo@users.noreply.github.com>
2019-10-11 11:50:26 -0700

    Give test folder a timestamped name (#102)
    Hoping this will fix #100 but we'll have to wait and see

--- tests/testthat/test-create-folder.R
+++ tests/testthat/test-create-folder.R
@@ -6,9 +6,17 @@ attempt_login()
 test_that("create_folder() creates a folder", {
   skip_if_not(logged_in())
 
+  op <- options(digits.secs = 3)
+  ## Create folder name with timestamp including milliseconds
+  name <- paste(
+    "my_test_folder",
+    format(Sys.time(), "%Y-%m-%d_%H.%M.%OS"),
+    sep = "_"
+  )
+  options(op) # reset digits options
   created_folder <- create_folder(
     parent = "syn17038062",
-    name = "my_test_folder"
+    name = name
   )
   expect_true(inherits(created_folder, "Folder"))
   on.exit(synDelete(created_folder)) # delete on exit
