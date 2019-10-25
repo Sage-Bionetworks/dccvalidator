f511bf55384c0df4d3af72ded39618b2c8ca8159
Aryllen <nmekauer@uw.edu>
2019-10-25 12:29:17 -0700

    Change order of data for check_ids_match with manifest
    Bidirectional checks the second set of data against the first set of data. We want to check the individual/specimen IDs against those in the manifest. Switching around makes that work.

--- R/app-server.R
+++ R/app-server.R
@@ -220,8 +220,8 @@ app_server <- function(input, output, session) {
     })
     individual_ids_indiv_manifest <- reactive({
       check_indiv_ids_match(
-        indiv(),
         manifest(),
+        indiv(),
         "individual",
         "manifest",
         bidirectional = FALSE
@@ -232,8 +232,8 @@ app_server <- function(input, output, session) {
     })
     specimen_ids_biosp_manifest <- reactive({
       check_specimen_ids_match(
-        biosp(),
         manifest(),
+        biosp(),
         "biospecimen",
         "manifest",
         bidirectional = FALSE
