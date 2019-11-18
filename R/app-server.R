59b08375e23264ddc7e622b6cfb15401f99888f1
kelshmo <kelsey.montgomery@sagebase.org>
2019-11-18 13:52:58 -0800

    store output in vignettes folder

--- R/app-server.R
+++ R/app-server.R
@@ -12,7 +12,12 @@ app_server <- function(input, output, session) {
   ## Render vignette markdown
   output$markdown <- renderUI({
     HTML(markdown::markdownToHTML(
-      knitr::knit(config::get("path_to_markdown"), 
+      knitr::knit(input = glue::glue(
+        file_path_sans_ext(config::get("path_to_markdown")), ".Rmd"
+        ),
+        output = glue::glue(
+          file_path_sans_ext(config::get("path_to_markdown")), ".md"
+        ),
                   quiet = TRUE),
       stylesheet = ("inst/app/www/custom.css")))
   })
