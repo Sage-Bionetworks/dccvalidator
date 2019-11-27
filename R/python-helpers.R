31f334aec4d289e9d69cf8fbb21504f1cd1e0859
Kara Woo <kara.woo@sagebase.org>
2019-11-26 16:28:06 -0800

    Fix dict_to_list

--- R/python-helpers.R
+++ R/python-helpers.R
@@ -1,5 +1,8 @@
 ## Convert dict of annotations (returned by syn$getAnnotations()) into a named
 ## list
 dict_to_list <- function(x) {
-  stats::setNames(x$values(), x$keys())
+  ## Not sure why x$values() doesn't work here, but it doesn't so we're
+  ## iterating to get the values instead :-/
+  values <- purrr::map(names(x), function(y) x$get(y))
+  stats::setNames(values, names(x))
 }
