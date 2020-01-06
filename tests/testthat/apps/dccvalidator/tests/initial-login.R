592facedf0c90aa8e346b195dff731f9afabd189
Nicole Kauer <nmekauer@gmail.com>
2020-01-06 14:53:16 -0800

    Add timeout to shiny app test

--- tests/testthat/apps/dccvalidator/tests/initial-login.R
+++ tests/testthat/apps/dccvalidator/tests/initial-login.R
@@ -1,4 +1,6 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("initial-login")
 
+# Set javascript expression to false so waits until timeout
+app$waitFor("false", timeout = 10000)
 app$snapshot()
