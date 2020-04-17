912b7409b429feeb088588f11c296d419c7ba7e7
Kara Woo <kara.woo@sagebase.org>
2020-04-17 14:46:52 -0700

    Add tests

--- tests/testthat/test-check-duplicate-files.R
+++ tests/testthat/test-check-duplicate-files.R
@@ -26,3 +26,14 @@ test_that("check_duplicate_paths() returns check_warn() if no path col", {
   dat <- data.frame(foo = 1:2)
   expect_true(inherits(check_duplicate_paths(dat), "check_warn"))
 })
+
+test_that("check_duplicate_paths() does not include NAs in resulting data", {
+  dat <- data.frame(path = c("foo.txt", "foo.txt", NA, NA))
+  res <- check_duplicate_paths(dat)
+  expect_equal(res$data, "foo.txt")
+})
+
+test_that("check_duplicate_paths() returns check_pass if all NAs", {
+  dat <- data.frame(path = c(NA, NA))
+  expect_true(inherits(check_duplicate_paths(dat), "check_pass"))
+})
