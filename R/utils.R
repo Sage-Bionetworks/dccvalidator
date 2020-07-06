a200971d05ac861d435ef8a89a22066a82795900
Kara Woo <kara.woo@sagebase.org>
2020-07-06 13:55:29 -0700

    Update CI login helper functions
    Rather than checking whether they run on travis, check whether they're run on
    CI. Both travis and github actions have a "CI" env var set to "true".

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 ## Check if we are running on travis
-on_travis <- function() {
-  if (identical(Sys.getenv("TRAVIS"), "true")) {
+on_ci <- function() {
+  if (identical(Sys.getenv("CI"), "true")) {
     return(TRUE)
     # nocov start
   } else {
@@ -10,8 +10,8 @@ on_travis <- function() {
 }
 
 ## Look up env vars and log in to Synapse
-syn_travis_login <- function(syn) {
-  ## Credentials are encrypted on travis
+syn_ci_login <- function(syn) {
+  ## Credentials are encrypted
   user <- Sys.getenv("SYNAPSE_USER")
   pass <- Sys.getenv("SYNAPSE_PASSWORD")
   syn$login(email = user, password = pass)
@@ -29,8 +29,8 @@ attempt_instantiate <- function() {
 ## org, or with regular synLogin() if not on travis. If on travis but not within
 ## Sage, do nothing.
 attempt_login <- function(syn, ...) {
-  if (on_travis() & !is.null(syn)) {
-    try(syn_travis_login(syn), silent = TRUE)
+  if (on_ci() & !is.null(syn)) {
+    try(syn_ci_login(syn), silent = TRUE)
   } else if (reticulate::py_module_available("synapseclient") & !is.null(syn)) {
     syn$login(...)
   } else {
