c80f506a429bbf799f7785e0a1106a7f627bd6bc
Nicole Kauer <nmekauer@gmail.com>
2021-05-20 12:58:56 -0700

    Try log in with sessionToken, else authToken

--- R/app-server.R
+++ R/app-server.R
@@ -40,28 +40,28 @@ app_server <- function(input, output, session) {
   observeEvent(input$cookie, {
     is_logged_in <- FALSE
     ## Move log in via sessionToken to a simple try without error handling
-    # try({
-    #     syn$login(sessionToken = input$cookie)
-    #     is_logged_in <- TRUE
-    #   },
-    #   silent = TRUE
-    # )
+    try({
+        syn$login(sessionToken = input$cookie, silent = TRUE)
+        is_logged_in <- TRUE
+      },
+      silent = TRUE
+    )
     ## Use authToken and handle error here if still not logged in
     if (!is_logged_in) {
       tryCatch({
-        syn$login(authToken = input$cookie)
-        is_logged_in <- TRUE
-      },
-      error = function(err) {
-        showModal(
-          modalDialog(
-            title = "Login error",
-            HTML("There was an error with the login process. Please refresh your Synapse session by logging out of and back in to <a target=\"_blank\" href=\"https://www.synapse.org/\">Synapse</a>. Then refresh this page to use the application."), # nolint
-            footer = NULL
+          syn$login(authToken = input$cookie, silent = TRUE)
+          is_logged_in <- TRUE
+        },
+        error = function(err) {
+          showModal(
+            modalDialog(
+              title = "Login error",
+              HTML("There was an error with the login process. Please refresh your Synapse session by logging out of and back in to <a target=\"_blank\" href=\"https://www.synapse.org/\">Synapse</a>. Then refresh this page to use the application."), # nolint
+              footer = NULL
+            )
           )
-        )
-      }
-      ) 
+        }
+      )
     }
     req(is_logged_in)
 
