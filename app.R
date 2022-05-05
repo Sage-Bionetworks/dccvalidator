9fd54afae7b2bd8dc446dd36828f7afbe7af2e3a
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-05-05 14:35:34 -0700

    delete package files

--- app.R
+++
@@ -1,8 +0,0 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
-
-Sys.setenv(R_CONFIG_ACTIVE = "default") # Replace "default" with your config
-pkgload::load_all()
-options("golem.app.prod" = TRUE)
-dccvalidator::run_app() # add parameters here (if any)
--- tests/testthat.R
+++
@@ -1,7 +0,0 @@
-library(testthat)
-library(dccvalidator)
-library(mockery)
-library(writexl)
-library(dplyr)
-
-test_check("dccvalidator")
